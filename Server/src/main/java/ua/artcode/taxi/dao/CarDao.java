d770841fc008a39451e80e2556d8e8dbb3ea3736
nick.no.cave@gmail.com <nick.no.cave@gmail.com>
2016-06-28 18:40:45 +0300

    add some methods to UserJdbcDao, OrderJdbcDao, AddressDao, CarDao + add log id to address, car

--- Server/src/main/java/ua/artcode/taxi/dao/CarDao.java
+++ Server/src/main/java/ua/artcode/taxi/dao/CarDao.java
@@ -1,41 +1,4 @@
 package ua.artcode.taxi.dao;
 
-import ua.artcode.taxi.model.Car;
-
-import java.util.List;
-
-/**
- * Created by ivan on 26.06.16.
- */
-public class CarDao  implements GenericDao{
-    @Override
-    public Object create(Object el) {
-        return null;
-    }
-
-    @Override
-    public boolean delete(Object el) {
-        return false;
-    }
-
-    @Override
-    public Car findById(int id) {
-
-        return null;
-    }
-
-    @Override
-    public List getAll(int offset, int length) {
-        return null;
-    }
-
-    @Override
-    public Object update(Object el) {
-        return null;
-    }
-
-    @Override
-    public Object getLast() {
-        return null;
-    }
+public class CarDao {
 }
